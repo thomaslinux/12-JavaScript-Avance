21c0b073a931397154491f06157898ad658a8666
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-02-18 14:09:01 +0100

    ok

--- module_03/TP ISS Correction/script.js
+++ module_03/TP ISS Correction/script.js
@@ -1,30 +1,30 @@
-function getIssPosition() {
-    fetch('http://api.open-notify.org/iss-now.json')
-        .then(httpResponse => httpResponse.json())
-        .then(data => {
-            document.getElementById('position').innerText = `Latitude = ${data.iss_position.latitude}, Longitude = ${data.iss_position.longitude}`;
-            map.flyTo([data.iss_position.latitude, data.iss_position.longitude], map.getZoom());
+let map, marker;
+function getIssPosition(){
 
-            marker.setLatLng([data.iss_position.latitude, data.iss_position.longitude]);
+    fetch('http://api.open-notify.org/iss-now.json')
+        .then((httpResponse) => httpResponse.json())
+        .then((data) => {
+            document.getElementById('position').innerText = `Latitude = ${data.iss_position.latitude}, Longitude = ${data.iss_position.longitude}`
+            map.flyTo([data.iss_position.latitude,data.iss_position.longitude])
+            marker.setLatLng([data.iss_position.latitude,data.iss_position.longitude])
         })
 }
-
-
-function init() {
-    let map = L.map('map').setView([0, 0], 4);
+function init(){
+    map = L.map('map').setView([48.03900045977704, -1.6923024930554964], 8);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-    let issIcon = L.icon({
-        iconUrl : './assets/iss.png',
-        iconSize: [48,48]
-    })
 
-    let marker = L.marker([0, 0], {icon: issIcon}).addTo(map);
-
-    setInterval(getIssPosition, 2000)
+    var issIcon = L.icon({
+        iconUrl: 'assets/iss.png',
+        iconSize:     [54, 54], // size of the icon
+    });
+    marker = L.marker([48.03900045977704, -1.6923024930554964], {icon: issIcon}).addTo(map);
+    setInterval(getIssPosition, 1000)
 }
 
-window.onload = init;
+window.onload = init
+
+

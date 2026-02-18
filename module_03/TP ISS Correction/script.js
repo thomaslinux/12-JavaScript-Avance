ccb568165cf0132b5f9932e0bdbe7fe6c9c6048f
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-02-18 13:40:59 +0100

    récupérer la data

--- module_03/TP ISS Correction/script.js
+++ module_03/TP ISS Correction/script.js
@@ -1,5 +1,9 @@
 function getIssPosition() {
     fetch('http://api.open-notify.org/iss-now.json')
         .then(httpResponse => httpResponse.json())
-
+        .then(data => {
+            console.log(data);
+        })
 }
+
+getIssPosition()

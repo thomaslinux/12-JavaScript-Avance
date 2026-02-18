6fe5e3cd0aa91273d94e6c64cbf0926c52c874e3
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-02-18 14:06:19 +0100

    add the map

--- module_03/TP ISS Correction/script.js
+++ module_03/TP ISS Correction/script.js
@@ -2,9 +2,29 @@ function getIssPosition() {
     fetch('http://api.open-notify.org/iss-now.json')
         .then(httpResponse => httpResponse.json())
         .then(data => {
-            console.log(data);
             document.getElementById('position').innerText = `Latitude = ${data.iss_position.latitude}, Longitude = ${data.iss_position.longitude}`;
+            map.flyTo([data.iss_position.latitude, data.iss_position.longitude], map.getZoom());
+
+            marker.setLatLng([data.iss_position.latitude, data.iss_position.longitude]);
         })
 }
 
-getIssPosition()
+
+function init() {
+    let map = L.map('map').setView([0, 0], 4);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    let issIcon = L.icon({
+        iconUrl : './assets/iss.png',
+        iconSize: [48,48]
+    })
+
+    let marker = L.marker([0, 0], {icon: issIcon}).addTo(map);
+
+    setInterval(getIssPosition, 2000)
+}
+
+window.onload = init;

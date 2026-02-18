47a1a510c95cbd30ddd60af448565ab537b4c5d7
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-02-18 10:24:59 +0100

    append characters to a div

--- module_03/demo02/script.js
+++ module_03/demo02/script.js
@@ -2,4 +2,20 @@ fetch("https://hp-api.onrender.com/api/characters")
     .then((httpResponse) => httpResponse.json())
     .then((data) => {
         console.log(data);
+
+        for(const character of data) {
+            const p = document.createElement("p");
+            p.innerText = character.name;
+            const img = document.createElement("img");
+            img.src = character.image;
+            img.style.height = "240px"
+
+            const div = document.createElement("div");
+            div.id = character.name;
+
+
+            div.append(p);
+            div.append(img);
+            document.body.append(div);
+        }
     })

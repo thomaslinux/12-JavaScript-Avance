1263d6fb9366edfdc5cff2080767bb7ab5a804cd
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-02-19 12:00:52 +0100

    add clarity spaces

--- module_04/demo03/script.ts
+++ module_04/demo03/script.ts
@@ -1,34 +1,40 @@
 class Chocolate implements Variety {
-    public cacaoLvl? : number
-    public origin : string
-    constructor(cacaoLvl : number, public color : string) {
+    public cacaoLvl?: number
+    public origin: string
+
+    constructor(cacaoLvl: number, public color: string) {
         this.cacaoLvl = cacaoLvl
     }
+
     color2: { nuance: string; origin: string; } = {
-        nuance : 'vert',
-        origin : 'Terre'
+        nuance: 'vert',
+        origin: 'Terre'
     };
     name: string = "";
+
     display() {
         console.log(`${this.cacaoLvl}% , Couleur : ${this.color}, Origin : ${this.origin}`);
     }
 }
+
 let choco = new Chocolate(90, 'black')
 choco.color = 'vert'
 choco.origin = 'Pérou'
 console.log(choco.color2.nuance)
 choco.display();
+
 interface Variety {
-    name : string
-    color2 : {
-        nuance : string
-        origin : string
+    name: string
+    color2: {
+        nuance: string
+        origin: string
     }
 }
-let crunch : Variety = {
-    name : "Crunch",
-    color2 : {
-        nuance : 'Marron bleu clair',
-        origin : 'Eurasien'
+
+let crunch: Variety = {
+    name: "Crunch",
+    color2: {
+        nuance: 'Marron bleu clair',
+        origin: 'Eurasien'
     }
 }

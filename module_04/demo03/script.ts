95e909a17d2598d13ade33aab011ad8f994c9525
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-02-19 12:00:34 +0100

    remove unnecessary spaces

--- module_04/demo03/script.ts
+++ module_04/demo03/script.ts
@@ -1,31 +1,23 @@
 class Chocolate implements Variety {
-
     public cacaoLvl? : number
     public origin : string
-
     constructor(cacaoLvl : number, public color : string) {
         this.cacaoLvl = cacaoLvl
     }
-
     color2: { nuance: string; origin: string; } = {
         nuance : 'vert',
         origin : 'Terre'
     };
-
     name: string = "";
-
     display() {
         console.log(`${this.cacaoLvl}% , Couleur : ${this.color}, Origin : ${this.origin}`);
     }
-
 }
-
 let choco = new Chocolate(90, 'black')
 choco.color = 'vert'
 choco.origin = 'Pérou'
 console.log(choco.color2.nuance)
 choco.display();
-
 interface Variety {
     name : string
     color2 : {
@@ -33,7 +25,6 @@ interface Variety {
         origin : string
     }
 }
-
 let crunch : Variety = {
     name : "Crunch",
     color2 : {
